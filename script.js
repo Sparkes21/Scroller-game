538c41916806101ed27ec6366f371f98a7894033
Josh Sparkes <jshsparx@gmail.com>
2022-06-06 12:17:06 -0600

    have vharacter movement, stuck at sprites

--- script.js
+++ script.js
@@ -1,7 +1,84 @@
-window.addEventListener('load', function(){
+window.addEventListener('load', () => {
   const canvas = document.getElementById('canvas1');
   // ctx = context (instance of built-in canvase 2d api that holds all drawing methods and properties we will need to animate our game)
   const ctx = canvas.getContext('2d');
   canvas.width = 800;
   canvas.height = 720;
+
+  class InputHandler {
+    constructor() {
+      this.keys = [];
+      window.addEventListener('keydown', (e) => {
+        if (( e.key === 'ArrowDown' || 
+              e.key === 'ArrowUp' || 
+              e.key === 'ArrowLeft' || 
+              e.key === 'ArrowRight') 
+              && this.keys.indexOf(e.key) === -1) {
+          this.keys.push(e.key);
+        }
+      });
+      window.addEventListener('keyup', (e) => {
+        if (  e.key === 'ArrowDown' || 
+              e.key === 'ArrowUp' || 
+              e.key === 'ArrowLeft' || 
+              e.key === 'ArrowRight') {
+            this.keys.splice(this.keys.indexOf(e.key), 1) ;
+        }
+      });
+    }
+  };
+
+  class Player {
+    constructor(gameWidth, gameHeight) {
+      this.gameWidth = gameWidth;
+      this.gameHeight = gameHeight;
+      this.width = 200;
+      this.height = 200;
+      this.x = 10;
+      this.y = this.gameHeight - this.height;
+      this.image = document.getElementById('playerImage');
+      this.frameX = 0;
+      this.frameY = 0;
+      this.speed = 0;
+    }
+    draw(context) {
+      context.fillStyle = 'white';
+      context.fillRect(this.x, this.y, this.width, this.height);
+      context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
+    }
+    update() {
+      this.x++;
+    }
+  };
+
+  class Background {
+
+  };
+
+  class Enemy {
+
+  
+  };
+
+  function handleEnemies() {
+
+  };
+
+  function displayStatusText() {
+
+  };
+
+  const input = new InputHandler();
+  const player = new Player(canvas.width, canvas.height);
+  
+
+
+  function animate() {
+    ctx.clearRect(0,0, canvas.width, canvas.height)
+    player.draw(ctx);
+    player.update();
+    requestAnimationFrame(animate);
+  };
+  animate();
+
 });
