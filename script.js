589747b5e92dc861842da4bad5006e9c458bebd1
Josh Sparkes <jshsparx@gmail.com>
2022-06-06 11:29:49 -0600

    basic setup for project

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function(){
+  const canvas = document.getElementById('canvas1');
+  // ctx = context (instance of built-in canvase 2d api that holds all drawing methods and properties we will need to animate our game)
+  const ctx = canvas.getContext('2d');
+  canvas.width = 800;
+  canvas.height = 720;
+});
