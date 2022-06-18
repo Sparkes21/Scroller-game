4b3046dca7952777f1bf71107f186927a644cee4
Josh Sparkes <jshsparx@gmail.com>
2022-06-18 13:16:15 -0600

    fix collision detection

--- script.js
+++ script.js
@@ -51,20 +51,13 @@ window.addEventListener('load', function() {
       this.weight = 1;
     }
     draw(context) {
-      //context.fillStyle = 'white';
-      //context.fillRect(this.x, this.y, this.width, this.height);
-      context.strokeStyle = 'white';
-      context.beginPath();
-      context.arc(this.x + this.width/2, this.y + this.height/2, this.width/2, 0, Math.PI * 2);
-      context.stroke();
-      context.strokeRect(this.x, this.y, this.width, this.height);
       context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
     }
     update(input, deltaTime, enemies) {
       //collision detection
       enemies.forEach(enemy => {
-        const dx = enemy.x - this.x;
-        const dy = enemy.y - this.y;
+        const dx = (enemy.x + enemy.width/2) - (this.x + this.width/2);
+        const dy = (enemy.y + enemy.height/2) - (this.y + this.height/2);
         const distance = Math.sqrt(dx* dx + dy * dy);
         if (distance < enemy.width/2 + this.width/2) {
           gameOver = true;
@@ -151,11 +144,6 @@ window.addEventListener('load', function() {
       this.markedForDeletion = false;
     }
     draw(context) {
-      context.strokeStyle = 'white';
-      context.strokeRect(this.x, this.y, this.width, this.height);
-      context.beginPath();
-      context.arc(this.x + this.width/2, this.y + this.height/2, this.width/2, 0, Math.PI * 2);
-      context.stroke();
       context.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height)
     }
     update(deltaTime) {
